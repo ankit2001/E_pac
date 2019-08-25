fa1d3312c1fdeb66e2249bb1766caba17b73fc27
ankit2001 <ankitchoudhary202.ac@gmail.com>
2019-08-25 11:45:51 +0530

    login page added

--- src/e_pac/e_pac/views.py
+++ src/e_pac/e_pac/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render 
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from .forms import ContactForm
+from .forms import ContactForm,LoginForm
 
 def home_page(request):
 	context={
@@ -29,3 +30,29 @@ def contact_page(request):
     #     print(request.POST.get('email'))
     #     print(request.POST.get('content'))
     return render(request, "contact/view.html", context)
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    #print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username  = form.cleaned_data.get("username")
+        password  = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        #print(request.user.is_authenticated())
+        if user is not None:
+            #print(request.user.is_authenticated())
+            login(request, user)
+            # Redirect to a success page.
+            #context['form'] = LoginForm()
+            return redirect("/")
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
+
+    return render(request, "auth/login.html", context)
